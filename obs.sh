8b33296a6801ab502a72e60ef479ccdc0ce138bc
THeAltPirate <37989051+bewarethealgorithm@users.noreply.github.com>
2018-07-23 15:44:30 -0400

    Add files via upload

---
+++ obs.sh
@@ -0,0 +1,5 @@
+sudo add-apt-repository ppa:kirillshkrogalev/ffmpeg-next && 
+sudo apt-get update && sudo apt-get install ffmpeg && 
+sudo add-apt-repository ppa:obsproject/obs-studio 
+apt-get update
+apt-get install obs-studio -y
